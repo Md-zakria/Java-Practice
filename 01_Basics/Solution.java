32f1ec7a54c89d9a365b5a8ff384a5eca40955d1
Muhammad Zakria <zakriajutt2917@gmail.com>
2026-04-21 02:06:04 +0500

    Add basic Hello World program in Java

---
+++ 01_Basics/Solution.java
@@ -0,0 +1,9 @@
+// This is every Java beginner's first program
+// The class name must match the file name: Solution.java
+
+public class Solution {
+    public static void main(String[] args) {
+        // just print the message and we're done
+        System.out.println("Hello, World!");
+    }
+}
